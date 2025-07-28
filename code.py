4051284fcf0f5d0850cde7a3853ddf5a23274ea2
Poojitha9983 <poojithakrovi@gmail.com>
2025-07-28 15:53:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,7 @@
-a=45
-b=45
-x=a+b
-<<<<<<< HEAD
-print(x)
+a=12
+b=13
+c=a+b
 print(a)
 print(b)
-=======
-print(x) #this is a addition program
->>>>>>> c36a4f6e9ab9f259b6f5e2a42f6d4afff8bfa8a7
+print(c)
+
